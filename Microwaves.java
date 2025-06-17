41dbc353635fc044453c65244b3534dbaf04ad0a
sahana390 <asahana390@gmail.com>
2025-06-17 19:31:17 +0530

    sop

---
+++ Microwaves.java
@@ -0,0 +1,12 @@
+class Microwaves{
+public static void main(String Panasonic[]){
+//message
+System.out.println("20L capacity: Suitable for bachelors & small families; power + innovation 800 watts of high power for fast, Even cooking and delicious results");
+System.out.println("Re-Heat & Defrost: Auto programmed reheat and defrost modes ensure even heating or defrosting of food without compromising on the textures of flavour.");
+System.out.println("Auto Cook 51 Menu: Choose from 51 preset recipes ranging from snacks to desserts to make every day meal a gourmet delight.");
+System.out.println("Vapor Clean: Keep oven odor free and stain free with the touch of a button");
+System.out.println("Compact design: This countertop microwave with Glass turntable is compact, allowing you to devote less space to electronics");
+System.out.println("Other Features: Touch Keypad, Digital Display");
+System.out.println("Also included in the box: turntable, rotating ring, user manual");
+}
+}
